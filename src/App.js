8f610a52923486ad7cc15c0dd8cbd6888242bd15
W3Collective <contact@w3collective.com>
2020-12-17 18:45:30 +1100

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className="App">                 
+        <ContactForm />           
+    </div>
+  );
+}
+
+export default App;
